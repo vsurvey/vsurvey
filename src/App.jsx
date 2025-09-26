0492b6591be5b57838d48aafc4aeec627e63eb62
Siranjeevan <smartjeevith2k@gmail.com>
2025-09-26 16:43:39 +0530

    Checking Client dashboard with out Login page

--- src/App.jsx
+++ src/App.jsx
@@ -60,7 +60,7 @@ function App() {
 
   return (
     <div className="min-h-screen bg-gray-50">
-      {session ? (
+      {!session ? (
         <Login onLogin={(type) => {
           setUserType(type)
           setSession(true)
