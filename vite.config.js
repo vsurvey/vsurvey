617c4afdaca5963f9d5b7ce2507c8364ba6c9b5e
KarthikRajS32 <karthikrajit32@gmail.com>
2025-09-30 16:31:35 +0530

    fix the 404 error

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/v-survey/',
+  base: "./", // ✅ use relative paths for Netlify
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
